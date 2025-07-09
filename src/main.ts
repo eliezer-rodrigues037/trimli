d6ca52a4d45eff1356f6baae8b30bdd08df8a034
eliezer-rodrigues037 <eliezer_gt.02@hotmail.com>
2025-07-09 17:38:22 -0300

    🔧 refactor: Update bootstrap function to use TrimliConfigService for dynamic port configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import { TrimliConfigService } from 'src/modules/trimli-config/trimli-config.service';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const trimliConfigService = app.get(TrimliConfigService);
+
+  await app.listen(trimliConfigService.get('PORT') ?? 3001);
 }
 bootstrap();
