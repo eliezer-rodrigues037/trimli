e8644c6f2b268ca94b006cd542faae0f4b5cf771
Eliezer Rodrigues <eliezer_gt.02@hotmail.com>
2025-07-13 17:37:06 -0300

    ✨ chore: Add Jest configuration file and update VSCode settings for improved testing setup

---
+++ jest-config.ts
@@ -0,0 +1,29 @@
+import dotenv from 'dotenv';
+import { JestConfigWithTsJest } from 'ts-jest';
+
+dotenv.config({ path: '.env.test', override: true });
+
+const jestConfig: JestConfigWithTsJest = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: './',
+  testEnvironment: 'node',
+  testRegex: '.e2e-spec.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  transformIgnorePatterns: ['/node_modules/'],
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/src/$1',
+    '^test/(.*)': '<rootDir>/test/$1',
+  },
+  modulePathIgnorePatterns: ['<rootDir>/dist'],
+  globals: {
+    PORT: 3000,
+    ACCESS_TOKEN:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIzNTNlYWFkMi0wZDI5LTQ0ZDgtOTkzNy00ODAzYzE4YWMwMzMiLCJlbWFpbCI6ImJvYkBkZXYuY29tIiwiaWF0IjoxNzUyNDM4NzAxLCJleHAiOjQ5MDgxOTg3MDF9.sylU5ELTXvOyxumYi4qpjN8SOVriJov8MTHHIJW5ZTQ',
+    EXPIRED_ACCESS_TOKEN: '',
+    INVALID_ACCESS_TOKEN: '',
+  },
+};
+
+export default jestConfig;
